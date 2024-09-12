31f017c08aa858046dd2b6ac1a416f0536ceb389
ZhidanRn <zhidanir@gmail.com>
2024-09-12 17:06:55 +0700

    update Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { NextAuthProvider } from "./components/NextAuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Netflix",
+  title: "Netflix - Watch TV Shows Online, Watch Movies Online",
   description: "Netflix is a streaming application to delivers a viewing experience akin to leading streaming platforms.",
 };
 
